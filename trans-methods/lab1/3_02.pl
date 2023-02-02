b27c4944a94d186767980a4255fc617016581974
letstatt <letstatt@outlook.com>
2023-02-02 21:56:18 +0300

    translation methods lab1

---
+++ trans-methods/lab1/3_02.pl
@@ -0,0 +1,30 @@
+$insert_blank_line = 0;
+$text_begins = 0;
+ 
+while (<>) {
+	# remove tags
+	s/<.*?>//g;
+	
+    if (/\S/) {
+        $text_begins = 1;
+    }
+    
+    if ($text_begins) {
+        # remove heading and trailing spaces
+        s/^\s+//;
+        s/\s+$//;
+        # shrink spaces
+        s/\s+/ /g;
+        
+        if (/^$/) {
+            $insert_blank_line = 1;
+        } else {
+            if ($insert_blank_line) {
+                $insert_blank_line = 0;
+                print "\n";
+            }
+            print;
+            print "\n";
+        }
+    }
+}
